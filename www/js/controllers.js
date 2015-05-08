c1bb45edca23f017e20c157e3f557197bbe02b2b
Philipe Felix <philipe.felix@abril.com.br>
2015-05-08 16:13:17 -0300

    Botões de status

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,30 +3,6 @@ angular.module('pokemon-score.controllers', [])
 
     $scope.pokemonList = [];
 
-    $scope.addPokemon = function(data) {
-      console.log(data);
-      $scope.pokemonList.push({
-        name: data.name,
-        psMax: data.ps,
-        ps: data.ps
-      });
-      data.name = '';
-      data.ps = '';
-      $scope.newPokemonModal.hide();
-    };
-
-    $scope.clearPokemon = function() {
-      $scope.pokemonList.splice(0,$scope.pokemonList.length);
-    }
-
-    $scope.damage = function(pokemon) {
-      pokemon.ps = pokemon.ps - 10;
-    };
-
-    $scope.heal = function(pokemon) {
-      pokemon.ps = pokemon.ps + 10;
-    };
-
     $ionicModal.fromTemplateUrl('new-pokemon.html', {
       scope: $scope,
       animation: 'slide-in-up'
@@ -51,5 +27,59 @@ angular.module('pokemon-score.controllers', [])
     $scope.$on('modal.removed', function() {
       // Execute action
     });
+
+    $scope.addPokemon = function(data) {
+      console.log(data);
+      $scope.pokemonList.push({
+        name: data.name,
+        psMax: data.ps,
+        ps: data.ps,
+        confused: false,
+        paralized: false,
+        asleep: false,
+        poisoned: false,
+        burnt: false
+      });
+      data.name = '';
+      data.ps = '';
+      $scope.newPokemonModal.hide();
+    };
+
+    $scope.clearPokemon = function() {
+      $scope.pokemonList.splice(0,$scope.pokemonList.length);
+    }
+
+    $scope.damage = function(pokemon) {
+      pokemon.ps = pokemon.ps - 10;
+    };
+
+    $scope.heal = function(pokemon) {
+      pokemon.ps = pokemon.ps + 10;
+    };
+
+    $scope.toggleConfused = function(pokemon) {
+      pokemon.confused = !pokemon.confused;
+      console.log(pokemon.confused);
+    };
+
+    $scope.toggleParalized = function(pokemon) {
+      pokemon.paralized = !pokemon.paralized;
+      console.log(pokemon.paralized);
+    };
+
+    $scope.toggleAsleep = function(pokemon) {
+      pokemon.asleep = !pokemon.asleep;
+      console.log(pokemon.asleep);
+    };
+
+    $scope.togglePoisoned = function(pokemon) {
+      pokemon.poisoned = !pokemon.poisoned;
+      console.log(pokemon.poisoned);
+    };
+
+    $scope.toggleBurnt = function(pokemon) {
+      pokemon.burnt = !pokemon.burnt;
+      console.log(pokemon.burnt);
+    };
 
   });
